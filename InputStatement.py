177f0c1caf2c16908c787a8d8e15697ec81cabe3
303pc12 <henrikhkvamme@gmail.com>
2017-11-01 20:45:51 +0100

    1st program

--- InputStatement.py
+++ InputStatement.py
@@ -6,6 +6,6 @@ print("Please enter the radius of the circle:")
 radius = float(input())
 print("You have entered a radius of", radius)
 
-circumferance = (2*myConstPi*radius)
+if(radius < 0):
+    print("Radius f the crcle cannot be a negatve number")
 print("The circumferance of the circle is:", 2*myConstPi*radius)
-print("The circumferance of the circle is:", circumferance)
