3107bd9bc4fd9c211d1fa6aaa170e7a573a68da2
303pc12 <henrikhkvamme@gmail.com>
2017-11-01 20:17:53 +0100

    Example of variavbles

---
+++ FunWithVariables.py
@@ -0,0 +1,11 @@
+#This is a variable.
+radius = 5
+
+#This is a constant
+myConstPi = 3.14
+
+print("The radius of the circle is", radius)
+
+print("The circumference of a circle is :", 2*myConstPi*radius)
+
+print("The circumference of  crcle as an integer s:", int(2*myConstPi*radius))
