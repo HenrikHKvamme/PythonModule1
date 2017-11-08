c6508096d8f7e4fcbc70a69cc70a1a3773c6ba25
303pc12 <henrikhkvamme@gmail.com>
2017-11-08 19:17:01 +0100

    Simple age calculator

---
+++ idk.py
@@ -0,0 +1,11 @@
+randomYear = int(input("Give me a year."))
+yearIWasBorn = int(input("Please enter the year whitch you were born."))
+
+print("My name is Henrik.")
+print("My age in", randomYear, "will be", randomYear - yearIWasBorn)
+
+#Tests
+# 1) Entered a string - breaks
+# 2) Entered a decimal - Breaks
+# 3) Empty string - breaks
+# 4) a space - breaks
