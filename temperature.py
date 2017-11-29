9a6890d751deec65280abe7b5f316d4e7927261e
303pc12 <henrikhkvamme@gmail.com>
2017-11-29 20:56:24 +0100

    calculating temperature with turtle

--- temperature.py
+++ temperature.py
@@ -1,11 +1,25 @@
-
-
+import turtle
 def celciusToFahrenheit():
-    myInput = int(input("Enter a temperature in Celcius: "))
+    myInput = float(input("Enter a temperature in Celcius: "))
     tempInFahrenheit = (myInput * 9 + 160) / 5
     print("The temperature of ", myInput, "in Celcius is ", tempInFahrenheit, "in Fahrenheit.")
+    myTurtle = turtle.Turtle()
+    sides = 4
+    while sides > 0:
+        myTurtle.forward(tempInFahrenheit)
+        myTurtle.left(90)
+        sides = sides - 1
+    turtle.done()
+
 
 def fahrenheitToCelcius():
-    myInput = int(input("Enter a temperature in Fahrenheit: "))
+    myInput = float(input("Enter a temperature in Fahrenheit: "))
     tempInCelcius = ((myInput-32)/9)*5
     print("The temperature of ", myInput, "in Fahrenheit is", tempInCelcius, "in Celcius.")
+    myTurtle = turtle.Turtle()
+    sides = 4
+    while sides > 0:
+        myTurtle.forward(tempInCelcius)
+        myTurtle.left(90)
+        sides = sides -1
+    turtle.done()
