de4653a92265f99ad6f0e81cbb8e200097f79abd
303pc12 <henrikhkvamme@gmail.com>
2017-11-29 20:57:18 +0100

    turtle testing

---
+++ turtle_square.py
@@ -0,0 +1,21 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+
+rounds = 1
+while rounds > 0:
+    myTurtle.forward(100)
+    myTurtle.left(135)
+    myTurtle.forward(141)
+    myTurtle.left(135)
+    myTurtle.forward(100)
+    myTurtle.left(90)
+    myTurtle.forward(100)
+    myTurtle.left(90)
+    myTurtle.forward(100)
+    myTurtle.left(90)
+    myTurtle.forward(100)
+    myTurtle.left(135)
+    myTurtle.forward(141)
+    rounds = rounds - 1
+turtle.done()
